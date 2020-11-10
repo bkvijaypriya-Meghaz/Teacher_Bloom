c7a770c5f823d363f5f892cefada99fcb33662d4
VictoryVP <victoryvp@users.noreply.github.com>
2020-11-10 17:49:14 +0530

    resolving ConstFile.js file

--- components/constFile.js
+++ components/constFile.js
@@ -1,1 +1,2 @@
 export const CLASS_AVERAGE = "https://bloomlms.azure-api.net/teacher/v1/34242443/34535535/class/average"
+export const STUDENT_ACTIVITY = "https://bloomlms.azure-api.net/teacher/v1/3242434/2342344/5675677/activity"
