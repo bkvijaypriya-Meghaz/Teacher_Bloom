66de5348c3be5101ba292ec25ca13f5bfbe6c06c
sai sathwik <saisathwik97@gmail.com>
2020-11-13 12:16:45 +0530

    story 49

--- components/ConstFile.js
+++ components/ConstFile.js
@@ -1,2 +1,3 @@
 export const STUDENT_ACTIVITY="https://bloomlms.azure-api.net/teacher/v1/3242434/2342344/5675677/activity"
 export const CLASS_AVERAGE = "https://bloomlms.azure-api.net/teacher/v1/34242443/34535535/class/average"
+export const CURRICULUM_CARDS="https://bloomlms.azure-api.net/teacher/v1/1231312/2342342/topics"
