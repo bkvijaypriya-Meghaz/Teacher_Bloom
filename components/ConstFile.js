52011474a427a8310d1ce5be328adc70e7edca55
VictoryVP <victoryvp@users.noreply.github.com>
2020-11-15 22:07:04 +0530

    ConstFile corrected

--- components/ConstFile.js
+++ components/ConstFile.js
@@ -8,6 +8,6 @@ export const CLASS_AVERAGE = "https://bloomlms.azure-api.net/teacher/v1/34242443
 export const CURRENT_RESPONSE ="https://bloomlms.azure-api.net/teacher/v1/2342342"
 export const ASSIGNMENT_TO_BE_GRADED="https://bloomlms.azure-api.net/teacher/v1/345353"
 export const STUDENT_ACTIVITY="https://bloomlms.azure-api.net/teacher/v1/3242434"
-export const CURRENT_RESPONSE ="https://bloomlms.azure-api.net/teacher/v1/2342342/2342342/assignments/current"
+//export const CURRENT_RESPONSE ="https://bloomlms.azure-api.net/teacher/v1/2342342/2342342/assignments/current"
 export const TEACHER_SECTIONS="https://bloomlms.azure-api.net/teacher/v1/23423424/sections"
 export const CURRICULUM_CARDS="https://bloomlms.azure-api.net/teacher/v1/1231312/2342342/topics"
