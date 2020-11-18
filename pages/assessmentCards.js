76363a1aab961dfb9b6b764901e8c3f90801b195
sai sathwik <saisathwik97@gmail.com>
2020-11-18 10:40:51 +0530

    story48

---
+++ pages/assessmentCards.js
@@ -0,0 +1,49 @@
+import React from 'react'
+export default function assessmentCards({ title, numberofquestions,date}) {
+    return (
+<div className="card user-card">
+                                        <div className="card-block">
+											<div className="row align-items-center justify-content-center">
+												<div className="col-auto">
+													<h5 className="m-b-15 text-uppercase lessonplanhead">{title}</h5>
+												</div>
+												<div className="col text-right assesstxt1">
+													{numberofquestions}
+												</div>
+											</div>
+                                            <span className="">Created On: <label className="label theme-bg f-12 f-w-400 text-white">{date}</label></span>
+											<div className="m-t-10">
+												<div className="row align-items-center justify-content-center">
+													<div className="col-auto">
+														<a href="viewAssessment" style={{color:'#888'}}>
+														<div className="assicon"><i className="feather icon-eye"></i></div>
+														<div className="asstxt">View</div>
+														</a>
+													</div>
+													<div className="col-auto">
+														<a href="editAssessment" style={{color:'#888'}}>
+														<div className="assicon"><i className="feather icon-edit-2"></i></div>
+														<div className="asstxt">edit</div>
+														</a>
+													</div>
+													<div className="col-auto">
+														<a href="copyAssessment" style={{color:'#888'}}>
+														<div className="assicon"><i className="feather icon-copy"></i></div>
+														<div className="asstxt">Copy</div>
+														</a>
+													</div>
+													<div className="col-auto" data-toggle="modal" data-target="#assignpopup">
+														<div className="assicon"><i className="feather icon-file-text"></i></div>
+														<div className="asstxt">Assign</div>
+													</div>
+													<div className="col-auto" data-toggle="modal" data-target="#deletepopup">
+														<div className="assicon"><i className="feather icon-trash-2"></i></div>
+														<div className="asstxt">Delete</div>
+													</div>
+												</div>
+											</div>
+                                        </div>
+                                    </div>
+    )
+}
+                                    
--- pages/assessments.js
+++ pages/assessments.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import TeacherLayout from '../layout/TeacherLayout';
 import {FiPlus} from  'react-icons/fi'
+import AssessmentCard from './assessmentCards'
+
 
 export default function assessments(props){
 
@@ -24,206 +26,30 @@ export default function assessments(props){
                                 
 								
                              <div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
-                             <div className="card user-card">
-                                        <div className="card-block">
-											<div className="row align-items-center justify-content-center">
-												<div className="col-auto">
-													<h5 className="m-b-15 text-uppercase lessonplanhead">Human Body Practice Test</h5>
-												</div>
-												<div className="col text-right assesstxt1">
-													20 Questions
-												</div>
-											</div>
-                                            <span className="">Created On: <label className="label theme-bg f-12 f-w-400 text-white">1st Jan 2020</label></span>
-											<div className="m-t-10">
-												<div className="row align-items-center justify-content-center">
-													<div className="col-auto">
-														
-														<div className="assicon"><i className="feather icon-eye"></i></div>
-														<div className="asstxt">View</div>
-														
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-edit-2"></i></div>
-														<div className="asstxt">edit</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-copy"></i></div>
-														<div className="asstxt">Copy</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-file-text"></i></div>
-														<div className="asstxt">Assign</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-trash-2"></i></div>
-														<div className="asstxt">Delete</div>
-													</div>
-												</div>
-											</div>
-                                        </div>
-                                    </div>
-                                </div>
+							 <AssessmentCard title='Human Body Practice Test' numberofquestions='20 Questions' date='1st Jan 2020'/>          
+							 </div>
+							 <div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
+							 <AssessmentCard title='Beginning of Year Test' numberofquestions='10 Questions' date='12th Jan 2019'/>          
+							 </div>
+							 <div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
+							 <AssessmentCard title='Energy Quiz' numberofquestions='5 Questions' date='1st Jan 2020'/>          
+							 </div>
+							 <div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
+							 <AssessmentCard title='Gravity &amp; Magnetism Test' numberofquestions='25 Questions' date='26th Feb 2020'/>          
+							 </div>
+							 <div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
+							 <AssessmentCard title='End of Year Test' numberofquestions='30 Questions' date='25th Dec 2019'/>          
+							 </div>
                                 
 								
-								<div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
-                                    <div className="card user-card">
-                                        <div className="card-block">
-											<div className="row align-items-center justify-content-center">
-												<div className="col-auto">
-													<h5 className="m-b-15 text-uppercase lessonplanhead">Beginning of Year Test</h5>
-												</div>
-												<div className="col text-right assesstxt1">
-													10 Questions
-												</div>
-											</div>
-                                            <span className="">Created On: <label className="label theme-bg f-12 f-w-400 text-white">12th Jan 2019</label></span>
-											<div className="m-t-10">
-												<div className="row align-items-center justify-content-center">
-													<div className="col-auto">
+								
 														
-														<div className="assicon"><i className="feather icon-eye"></i></div>
-														<div className="asstxt">View</div>
-														
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-edit-2"></i></div>
-														<div className="asstxt">edit</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-copy"></i></div>
-														<div className="asstxt">Copy</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-file-text"></i></div>
-														<div className="asstxt">Assign</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-trash-2"></i></div>
-														<div className="asstxt">Delete</div>
-													</div>
-												</div>
-											</div>
-                                        </div>
-                                    </div>
-                                </div>
-								<div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
-                                    <div className="card user-card">
-                                        <div className="card-block">
-											<div className="row align-items-center justify-content-center">
-												<div className="col-auto">
-													<h5 className="m-b-15 text-uppercase lessonplanhead">Energy Quiz</h5>
-												</div>
-												<div className="col text-right assesstxt1">
-													5 Questions
-												</div>
-											</div>
-                                            <span className="">Created On: <label className="label theme-bg f-12 f-w-400 text-white">1st Jan 2020</label></span>
-											<div className="m-t-10">
-												<div className="row align-items-center justify-content-center">
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-eye"></i></div>
-														<div className="asstxt">View</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-edit-2"></i></div>
-														<div className="asstxt">edit</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-copy"></i></div>
-														<div className="asstxt">Copy</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-file-text"></i></div>
-														<div className="asstxt">Assign</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-trash-2"></i></div>
-														<div className="asstxt">Delete</div>
-													</div>
-												</div>
-											</div>
-                                        </div>
-                                    </div>
-                                </div>
-								<div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
-                                    <div className="card user-card">
-                                        <div className="card-block">
-											<div className="row align-items-center justify-content-center">
-												<div className="col-auto">
-													<h5 className="m-b-15 text-uppercase lessonplanhead">Gravity &amp; Magnetism Test</h5>
-												</div>
-												<div className="col text-right assesstxt1">
-													25 Questions
-												</div>
-											</div>
-                                            <span className="">Created On: <label className="label theme-bg f-12 f-w-400 text-white">26th Feb 2020</label></span>
-											<div className="m-t-10">
-												<div className="row align-items-center justify-content-center">
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-eye"></i></div>
-														<div className="asstxt">View</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-edit-2"></i></div>
-														<div className="asstxt">edit</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-copy"></i></div>
-														<div className="asstxt">Copy</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-file-text"></i></div>
-														<div className="asstxt">Assign</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-trash-2"></i></div>
-														<div className="asstxt">Delete</div>
-													</div>
-												</div>
-											</div>
-                                        </div>
-                                    </div>
-                                </div>
-								<div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
-                                    <div className="card user-card">
-                                        <div className="card-block">
-											<div className="row align-items-center justify-content-center">
-												<div className="col-auto">
-													<h5 className="m-b-15 text-uppercase lessonplanhead">End of Year Test</h5>
-												</div>
-												<div className="col text-right assesstxt1">
-													30 Questions
-												</div>
-											</div>
-                                            <span className="">Created On: <label className="label theme-bg f-12 f-w-400 text-white">25th Dec 2019</label></span>
-											<div className="m-t-10">
-												<div className="row align-items-center justify-content-center">
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-eye"></i></div>
-														<div className="asstxt">View</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-edit-2"></i></div>
-														<div className="asstxt">edit</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-copy"></i></div>
-														<div className="asstxt">Copy</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-file-text"></i></div>
-														<div className="asstxt">Assign</div>
-													</div>
-													<div className="col-auto">
-														<div className="assicon"><i className="feather icon-trash-2"></i></div>
-														<div className="asstxt">Delete</div>
-													</div>
-												</div>
-											</div>
-                                        </div>
-                                    </div>
-                                </div>
+													
+                                
+							
+                                
+								
+								
 								<div className="col-md-6 col-xl-4 col-sm-12 col-xs-12">
                                 { /* <div className="card user-card" id="createlesson">
                                         <a href="">
