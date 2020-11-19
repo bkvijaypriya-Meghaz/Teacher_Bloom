6809115291b87e90cef62c83e9bda9a4bd677d2a
divyanair <divunair@gmail.com>
2020-11-19 10:57:19 +0900

    my classes changed according to new UI and added api integration for grade selection

---
+++ pages/attendanceTracking.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import BarChart from './dashboardBarChart';
+
+function attendanceTracking() {
+    return (
+        <div>
+            <h1>attendance tracking</h1>
+        </div>
+    )
+}
+
+export default attendanceTracking
