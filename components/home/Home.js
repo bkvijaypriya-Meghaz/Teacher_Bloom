37467eac75ad8dd50130ad83f93a77287a354482
bbachi <bhargavbachina@yahoo.com>
2020-11-28 12:18:21 -0600

    micro frontend changes

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,29 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import TeacherSignIn from '../../pages/teacher'
 
 //change 5 - try release pipeline - comment to test pipeline run - Jilan - 2020-11-02 09.35AM ist
 function Home() {
     
     
     return (
-        <div className="auth-wrapper">
-
-            <div className="auth-content subscribe">
-                <div className="card" style={{background: "transparent", boxShadow: "none"}}>
-                    <div className="row no-gutters justify-content-center">
-                        <div className="col-md-4 col-lg-6">
-							<a href="teacher">
-							<div className="theme-bg align-items-center justify-content-center loginouterdiv">
-								<div className="loginusericon">
-                                    <img src="../../assets/images/user/teachericon.png" alt="lock images" className="img-fluid" />
-								</div>
-								<div className="loginusertxt">Teacher</div>
-							</div>
-							</a>
-                        </div>
-					</div>
-                </div>
-            </div>
-        </div>
+        
+        <TeacherSignIn />
       );
 }
 
